232fc68dac3d0fdbe04f3ed2092247394de1672b
Rishikesh0930 <rishikeshkushwaha018@gmail.com>
2026-01-15 23:47:01 +0530

    Initial commit

---
+++ App/views.py
@@ -0,0 +1,31 @@
+from django.core.mail import send_mail
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+def home_page(request):
+    return render(request, 'home.html')
+
+def about_page(request):
+    return render(request, 'about.html')
+
+def skills_page(request):
+    return render(request, 'skills.html')
+
+def project_page(request):
+    return render(request, 'project.html')
+
+def contact_page(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        subject = request.POST.get("subject")
+        message = request.POST.get("message")
+        send_mail(
+            subject=f"{subject}",
+            message=f"From: {email}\n\nI'm {name}\n\n{message}",
+            from_email=email,
+            recipient_list=['rishikeshkushwaha181811@gmail.com'],
+            fail_silently=False,
+        )
+        messages.success(request, "Your message has been sent successfully!")
+    return render(request, 'contact.html')
