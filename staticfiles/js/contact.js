cd301562a89379306db75f4ad7be34407ad502f8
Rishikesh0930 <rishikeshkushwaha018@gmail.com>
2026-01-18 20:06:16 +0530

    update all files

--- staticfiles/js/contact.js
+++
@@ -1,9 +0,0 @@
-const message = document.getElementById("flash-message");
-if (message) {
-    setTimeout(() => {
-        message.style.opacity = "0";
-        setTimeout(() => {
-            message.style.display = "none";
-        }, 500);
-    }, 5000);
-}
