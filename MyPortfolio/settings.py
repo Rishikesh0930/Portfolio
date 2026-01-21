65ac701b518bf8edd058843c74e50277520ed66a
Rishikesh0930 <rishikeshkushwaha018@gmail.com>
2026-01-21 11:46:48 +0530

    update MyPortfolio

--- MyPortfolio/settings.py
+++ MyPortfolio/settings.py
@@ -131,6 +131,8 @@ STATICFILES_DIRS = [
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.sendgrid.net"  
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
