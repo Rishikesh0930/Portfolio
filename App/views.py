903dcd6b99a0e72e1defec54b1f2c2de8d290b39
Rishikesh0930 <rishikeshkushwaha018@gmail.com>
2026-01-22 01:28:24 +0530

    update views.py

--- App/views.py
+++ App/views.py
@@ -2,6 +2,8 @@ import os
 from django.shortcuts import render,redirect
 from django.contrib import messages
 import requests
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
 
 def home_page(request):
     return render(request, 'home.html')
@@ -42,16 +44,31 @@ def contact_page(request):
         email = request.POST.get("email", "")
         subject = request.POST.get("subject", "")
         message = request.POST.get("message", "")
-        text =(
-        f"📩 <b>Subject - </b>{subject}\n"
-        f"📧 <b>Email - </b> {email}\n"
-        f"👤 <b>Name - </b> {name}\n\n"
-        f"{message}"
-        )
-        success = send_telegram_message(text)
-        if success:
-            messages.success(request, "Message sent successfully!\nIf you don’t receive a reply within 1 hour,\nplease contact me via email.")
+        try:
+            validate_email(email)
+        except ValidationError:
+            messages.success(request, "Please enter a valid email address.")
+        if not all([name, email, subject, message]):
+            if not len(name)>3:
+                if not len(name)<30:
+                    if not len(subject)<100:
+                        if not len(message)>30:
+                            messages.success(request, "Message should be greater than 30 characters.")
+                        messages.success(request, "Subject should be less than 100 character.")
+                    messages.success(request, "Name should between 3 and 30 characters.")
+                messages.success(request, "Name should between 3 and 30 characters.")
+            messages.success(request, "All fields are required.")
         else:
-            messages.success(request, "Message failed. Try again later.")
-        return redirect("contact-page")
+            text =(
+            f"📩 <b>Subject - </b>{subject}\n"
+            f"📧 <b>Email - </b> {email}\n"
+            f"👤 <b>Name - </b> {name}\n\n"
+            f"{message}"
+            )
+            success = send_telegram_message(text)
+            if success:
+                messages.success(request, "Message sent successfully!\nIf you don’t receive a reply within 1 hour,\nplease contact me via email.")
+            else:
+                messages.success(request, "Message failed. Try again later.")
+            return redirect("contact-page")
     return render(request, 'contact.html')
